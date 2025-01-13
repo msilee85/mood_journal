7413967392a088ee81b3ae87b37e4dad06daeb2b
Michael <mlee85@gmail.com>
2025-01-13 09:30:07 -0800

    update journal entries

--- mood_analyzer/utils/api.ts
+++ mood_analyzer/utils/api.ts
@@ -15,3 +15,17 @@ export const createNewEntry = async() => {
   }
 
 }
+
+export const updateEntry = async(id, content) => {
+  const res = await fetch(
+    new Request(createURL(`api/journal/${id}`), {
+      method: 'PATCH',
+      body: JSON.stringify({ content })
+    })
+  )
+
+  if (res.ok) {
+    const data = await res.json()
+    return data.data
+  }
+}
