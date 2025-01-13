5c969e59305f68f9377eeec1ecb9c1d77e029d8a
Michael <mlee85@gmail.com>
2025-01-13 09:42:56 -0800

    downgrade react version

--- mood_analyzer/components/Editor.tsx
+++ mood_analyzer/components/Editor.tsx
@@ -11,7 +11,8 @@ const Editor = ({ entry }) => {
       <textarea
         className="w-full h-full p-8 text-xl outline-none"
         value={value}
-        onChange={(e) => setValue(e.target.value)}/>
+        onChange={(e) => setValue(e.target.value)}
+      />
     </div>
   )
 }
