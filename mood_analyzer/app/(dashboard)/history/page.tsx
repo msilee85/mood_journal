2dad7ba5986d8df09b9f218fde0e4f35268edfc2
Michael <mlee85@gmail.com>
2025-01-13 21:15:05 -0800

    add history chart

--- mood_analyzer/app/(dashboard)/history/page.tsx
+++ mood_analyzer/app/(dashboard)/history/page.tsx
@@ -1,7 +1,35 @@
-const History = () => {
+import HistoryChart from "@/components/HistoryChart"
+import { getUserByClerkID } from "@/utils/auth"
+import { prisma } from "@/utils/db"
+
+const getData = async () => {
+  const user = await getUserByClerkID()
+  const analyses = await prisma.analysis.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: 'asc'
+    }
+  })
+
+  const sum = analyses.reduce((all, current) => all + current.sentimentScore, 0)
+  const avg = Math.round(sum / analyses.length)
+  return { analyses, avg}
+
+}
+
+const History = async () => {
+  const { avg, analyses } = await getData()
+
   return (
-    <div>
-      History
+    <div className="w-full h-full">
+      <div>
+        {`Avg. Sentiment ${avg}`}
+      </div>
+      <div className="h-full w-full">
+        <HistoryChart data={analyses}/>
+      </div>
     </div>
   )
 }
