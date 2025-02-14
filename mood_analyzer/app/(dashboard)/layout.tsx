b75f88f07f0dc90ef54fe86060bde6e18037de65
Michael <mlee85@gmail.com>
2025-02-14 11:30:17 -0800

    fix width in dashboard layot

--- mood_analyzer/app/(dashboard)/layout.tsx
+++ mood_analyzer/app/(dashboard)/layout.tsx
@@ -1,26 +1,21 @@
-import { UserButton } from "@clerk/nextjs"
-import Link from "next/link"
+import { UserButton } from '@clerk/nextjs';
+import Link from 'next/link';
 
 const links = [
   { href: '/', label: 'Home' },
   { href: '/journal', label: 'Journal' },
-  { href: '/history', label: 'History' }
-]
-
+  { href: '/history', label: 'History' },
+];
 
 const DashboardLayout = ({ children }) => {
   return (
-    <div className="h-screen w-screen relative">
+    <div className="h-full w-full relative">
       <aside className="absolute w-[200px] top-0 left-0 h-full border-r border-black/10">
-        <div>
-          Mood
-        </div>
+        <div>Mood</div>
         <ul>
-          {links.map((link) =>(
+          {links.map((link) => (
             <li key={link.href} className="px-2 py-6 text-xl">
-              <Link href={link.href}>
-                {link.label}
-              </Link>
+              <Link href={link.href}>{link.label}</Link>
             </li>
           ))}
         </ul>
@@ -28,13 +23,13 @@ const DashboardLayout = ({ children }) => {
       <div className="ml-[200px] h-full">
         <header className="h-[60px] border-b border-black/10">
           <div className="h-full w-full px-6 flex items-center justify-end">
-            <UserButton/>
+            <UserButton />
           </div>
         </header>
         <div className="h-[calc(100vh-60px)]">{children}</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DashboardLayout
+export default DashboardLayout;
